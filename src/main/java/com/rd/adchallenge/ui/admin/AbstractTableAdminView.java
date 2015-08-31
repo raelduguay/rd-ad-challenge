9df24231981ce4ab11b55c4a7b6cf0b6fcd27ba6
raelduguay <rael@Jocelyns-MacBook-Pro.local>
2015-08-30 20:43:10 -0400

    Cleanup  irrelevant file headers

--- src/main/java/com/rd/adchallenge/ui/admin/AbstractTableAdminView.java
+++ src/main/java/com/rd/adchallenge/ui/admin/AbstractTableAdminView.java
@@ -1,18 +1,6 @@
 /* 
- * ---------------------------------------------------------------------------
- *
- * COPYRIGHT (c) 2015 Nuance Communications Inc. All Rights Reserved.
- *
- * The copyright to the computer program(s) herein is the property of
- * Nuance Communications Inc. The program(s) may be used and/or copied
- * only with the written permission from Nuance Communications Inc.
- * or in accordance with the terms and conditions stipulated in the
- * agreement/contract under which the program(s) have been supplied.
- *
  * Author: rael
  * Date  : Aug 29, 2015
- *
- * ---------------------------------------------------------------------------
  */
 package com.rd.adchallenge.ui.admin;
 
--- src/main/java/com/rd/adchallenge/ui/app/AppUi.java
+++ src/main/java/com/rd/adchallenge/ui/app/AppUi.java
@@ -1,18 +1,6 @@
 /* 
- * ---------------------------------------------------------------------------
- *
- * COPYRIGHT (c) 2015 Nuance Communications Inc. All Rights Reserved.
- *
- * The copyright to the computer program(s) herein is the property of
- * Nuance Communications Inc. The program(s) may be used and/or copied
- * only with the written permission from Nuance Communications Inc.
- * or in accordance with the terms and conditions stipulated in the
- * agreement/contract under which the program(s) have been supplied.
- *
  * Author: rael
  * Date  : Aug 28, 2015
- *
- * ---------------------------------------------------------------------------
  */
 package com.rd.adchallenge.ui.app;
 
@@ -28,8 +16,6 @@ import com.vaadin.ui.VerticalLayout;
 @SpringUI(path="/app")
 @Title(AppUi.TITLE)
 public class AppUi extends UI {
-  // TODO RD HERE Final refactorings (naming, consistency)
-  // TODO RD HERE Publish on Heroku or similar...
 
   private static final long serialVersionUID = 8477406446716066381L;
   
